bb92e5f3bd6ae6a9f917ff7672c3d459443acde1
ShobhanSrivastava <shobhansrivastava.200121067@gehu.ac.in>
2021-09-24 17:30:53 +0530

    Insertion Sort Added

---
+++ Algorithms/InsertionSort.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] arr = { -4, -5, -19, 0, 45, 2, 4, -10 };
+
+        InsertionSort(arr);
+
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+
+    static void InsertionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j > 0; j--) {
+                if (arr[j - 1] > arr[j]) {
+                    swap(arr, j - 1, j);
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+}
