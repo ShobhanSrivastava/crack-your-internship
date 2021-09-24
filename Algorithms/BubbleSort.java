5adfa69a922edf36a76e74140174c8895665a916
ShobhanSrivastava <shobhansrivastava.200121067@gehu.ac.in>
2021-09-24 17:20:16 +0530

    More algos added

---
+++ Algorithms/BubbleSort.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {-4, -5, -19, 0, 45, 2, 4, -10};
+        
+        bubbleSort(arr);
+
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+
+    static void bubbleSort(int[] arr){
+        boolean swapped = false;
+        for(int i=0 ; i<arr.length ; i++){
+            for(int j=1 ; j<arr.length-i ; j++){
+                if(arr[j] < arr[j-1]){
+                    swap(arr,j-1,j);
+                    swapped = true;
+                }
+            }
+            if(!swapped){
+                break;
+            }
+        }
+    }
+}
---
+++ Algorithms/SelectionSort.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = { -4, -5, -19, 0, 45, 2, 4, -10 };
+
+        selectionSort(arr);
+
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+
+    static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int max = 0;
+            for (int j = 0; j < arr.length - i; j++) {
+                if (arr[max] < arr[j]) {
+                    max = j;
+                }
+            }
+            swap(arr, max, arr.length-1-i);
+        }
+    }
+}
