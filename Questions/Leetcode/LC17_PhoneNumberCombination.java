21412172d67cd011ce8d5b70f502ce1a32292d06
Shobhan <srivastavashobhan2001@gmail.com>
2022-01-07 01:28:50 +0530

    LC_17 Phone Number Combinations

---
+++ Questions/Leetcode/LC17_PhoneNumberCombination.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class LC17_PhoneNumberCombination {
+    public static void main(String[] args) {
+        String[] numberArr = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        System.out.println(phoneNumberCombinations("", "4526", numberArr));
+    }
+
+    static ArrayList<String> phoneNumberCombinations(String p, String digits, String[] numberArr){
+        if(digits.isEmpty()){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+
+        int digit = (int)digits.charAt(0);
+        System.out.println(digit);
+        ArrayList<String> list = new ArrayList<>();
+
+        String digitMap = numberArr[digit - 50];
+        for(int i=0 ; i < digitMap.length() ; i++){
+            list.addAll(phoneNumberCombinations(p + digitMap.charAt(i), digits.substring(1), numberArr));
+        }
+        return list;
+    }
+}
