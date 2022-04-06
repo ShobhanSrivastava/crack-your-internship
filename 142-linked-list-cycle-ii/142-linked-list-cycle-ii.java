690faf6f9aee3b8c0871727127eb52acaf88b7ad
ShobhanSrivastava <76567059+ShobhanSrivastava@users.noreply.github.com>
2022-04-06 17:28:05 +0530

    Time: 0 ms (100.00%), Space: 45.1 MB (45.12%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.java
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.java
@@ -1,28 +1,56 @@
-/**
- * Definition for singly-linked list.
- * class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
- * }
- */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        HashMap<ListNode, Integer> map = new HashMap<>();
+        ListNode commonNode = findNodeInCycle(head);
         
-        while(head != null){
-            if(map.get(head) == null){
-                map.put(head, 1);
-            }
-            else{
-                return head;
-            }
+        if(commonNode == null){
+            return null;
+        }
+        
+        // int length = findLength(commonNode, commonNode);
+        
+//         ListNode first = head, second = head;
+        
+//         for(int i=1 ; i<=length ; i++){
+//             first = first.next;
+//         }
+        
+//         while(first != second){
+//             first = first.next;
+//             second = second.next;
+//         }
+        
+//         return first;
+        
+        while(commonNode != head){
+            commonNode = commonNode.next;
             head = head.next;
+        }
+        
+        return head;
+    }
+    
+    public ListNode findNodeInCycle(ListNode head){
+        ListNode slow = head, fast = head;
+        
+        while(slow != null && fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            
+            if(slow == fast){
+                return slow;
+            }
         }
         
         return null;
     }
+    
+//     public int findLength(ListNode first, ListNode second){
+//         int count = 0;
+//         while(first != second){
+//             first = first.next;
+//             count++;
+//         }
+        
+//         return count;
+//     }
 }
