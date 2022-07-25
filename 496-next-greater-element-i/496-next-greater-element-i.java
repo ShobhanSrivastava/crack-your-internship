59b4321a145f7a2cd41c2899ed427548c26d258d
ShobhanSrivastava <76567059+ShobhanSrivastava@users.noreply.github.com>
2022-07-26 00:18:21 +0530

    Time: 9 ms (18.04%), Space: 44.4 MB (35.83%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.java
+++ 496-next-greater-element-i/496-next-greater-element-i.java
@@ -1,27 +1,37 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        HashMap<Integer, Integer> indexMap = new HashMap<>();
-        // HasmMap<Integer, Integer> greaterMap = new HashMap<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
+        Stack<Integer> stack = new Stack<>();
         
-        int greater = nums2[nums2.length-1];
-        for(int i=nums2.length-1 ; i>=0 ; i--){
-            indexMap.put(nums2[i], i);
+        for(int i=0 ; i<nums2.length ; i++){
+            map.put(nums2[i], i);
+            
+            if(stack.empty()){
+                stack.push(i);
+            }
+            else{
+                // if(nums2[i] > nums2[top]){
+                while(!stack.empty() && nums2[i] > nums2[stack.peek()]){
+                    nums2[stack.peek()] = nums2[i];
+                    stack.pop();
+                }
+                
+                stack.push(i);
+                // }
+                // else{
+                //     stack.push(i);
+                // }
+            }
         }
         
-        for(int j=0 ; j<nums1.length ; j++){
-            int index = indexMap.get(nums1[j]);
-            boolean found = false;
-            for(int k=index+1 ; k<nums2.length ; k++){
-                if(nums2[k] > nums1[j]){
-                    found = true;
-                    nums1[j] = nums2[k];
-                    break;
-                }
-            }
-            
-            if(!found){
-                nums1[j] = -1;
-            }
+        while(!stack.empty()){
+            nums2[stack.peek()] = -1;
+            stack.pop();
+        }
+        
+        for(int i=0 ; i<nums1.length ; i++){
+            int index = map.get(nums1[i]);
+            nums1[i] = nums2[index];
         }
         
         return nums1;
