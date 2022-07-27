6a922bdd785f9e284636eceeeaf7b2b9976206c0
ShobhanSrivastava <76567059+ShobhanSrivastava@users.noreply.github.com>
2022-07-27 15:37:47 +0530

    Time: 236 ms (16.42%), Space: 131.8 MB (39.98%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.java
+++ 739-daily-temperatures/739-daily-temperatures.java
@@ -1,53 +1,23 @@
-// class Solution {
-//     public int[] dailyTemperatures(int[] temperatures) {
-//         Stack<Integer> stack = new Stack<>();
-//         int[] result = new int[temperatures.length];
-//         for(int i = 0; i < temperatures.length; i++) {
-//             while(!stack.isEmpty() && temperatures[stack.peek()] < temperatures[i]) {
-//                 int popped = stack.pop();
-//                 result[popped] = i-popped;
-//             }
-//             stack.push(i);
-//         }
-//         return result;
-//     }
-// }
-
 class Solution {
     public int[] dailyTemperatures(int[] temp) {
         Stack<int[]> st = new Stack<>();
         
         for(int i=0 ; i<temp.length ; i++){
-            if(st.empty()){
-                st.push(newArr(temp[i], i));
+            while(!st.empty() && st.peek()[0]<temp[i]){
+                int index = st.peek()[1];
+                temp[index] = i-index;
+                st.pop();
             }
-            else{
-                if(st.peek()[0] > temp[i]){
-                    st.push(newArr(temp[i], i));
-                }
-                else{
-                    int popCount = 0;
-                    while(!st.empty() && st.peek()[0] < temp[i]){
-                        int[] data = st.peek();
-                        temp[data[1]] = i-data[1];
-                        st.pop();
-                    }
-                    
-                    st.push(newArr(temp[i], i));
-                }
-            }
+            
+            st.push(new int[]{temp[i], i});
         }
         
         while(!st.empty()){
-            int[] data = st.peek();
-            temp[data[1]] = 0;
+            int index = st.peek()[1];
+            temp[index] = 0;
             st.pop();
         }
         
         return temp;
-    }
-    
-    public int[] newArr(int temp, int index){
-        return new int[]{temp, index};
     }
 }
