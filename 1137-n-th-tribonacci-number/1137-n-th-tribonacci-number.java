b06bdbec3923338ac353682613eb1474c17417a4
ShobhanSrivastava <76567059+ShobhanSrivastava@users.noreply.github.com>
2022-08-05 00:43:25 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (79.00%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -1,25 +1,25 @@
 class Solution {
     public int tribonacci(int n) {
-        Integer[] qb = new Integer[n+1];
+        int[] qb = new int[n+1];
         
         return tribo(n, qb);
     }
     
-    public int tribo(int n, Integer[] qb){
+    public int tribo(int n, int[] qb){
         if(n == 0 || n == 1){
             return n;
         }
-        else if(n == 2){
+        
+        if(n == 2){
             return 1;
         }
-        else{
-            if(qb[n] != null){
-                return qb[n];
-            }
-            
-            qb[n] = tribo(n-1, qb) + tribo(n-2, qb) + tribo(n-3, qb);
-            
+        
+        if(qb[n] != 0){
             return qb[n];
         }
+        
+        qb[n] = tribo(n-1, qb) + tribo(n-2, qb) + tribo(n-3, qb);
+        
+        return qb[n];
     }
 }
