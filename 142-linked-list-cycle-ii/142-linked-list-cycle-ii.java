b3b1825206fda6baf99033151ca008dcbd985783
ShobhanSrivastava <76567059+ShobhanSrivastava@users.noreply.github.com>
2022-09-02 11:39:42 +0530

    Time: 0 ms (100.00%), Space: 45.1 MB (48.08%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.java
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.java
@@ -1,56 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        ListNode commonNode = findNodeInCycle(head);
+        ListNode slow = head, fast = head;
+        boolean cycle = false;
         
-        if(commonNode == null){
+        while(fast!=null && fast.next!=null && fast.next.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+            
+            if(slow == fast){
+                cycle = true;
+                break;
+            }
+        }
+        
+        if(!cycle){
             return null;
         }
         
-        // int length = findLength(commonNode, commonNode);
-        
-//         ListNode first = head, second = head;
-        
-//         for(int i=1 ; i<=length ; i++){
-//             first = first.next;
-//         }
-        
-//         while(first != second){
-//             first = first.next;
-//             second = second.next;
-//         }
-        
-//         return first;
-        
-        while(commonNode != head){
-            commonNode = commonNode.next;
+        while(slow!=head){
+            slow = slow.next;
             head = head.next;
         }
         
         return head;
     }
-    
-    public ListNode findNodeInCycle(ListNode head){
-        ListNode slow = head, fast = head;
-        
-        while(slow != null && fast != null && fast.next != null){
-            slow = slow.next;
-            fast = fast.next.next;
-            
-            if(slow == fast){
-                return slow;
-            }
-        }
-        
-        return null;
-    }
-    
-//     public int findLength(ListNode first, ListNode second){
-//         int count = 0;
-//         while(first != second){
-//             first = first.next;
-//             count++;
-//         }
-        
-//         return count;
-//     }
 }
