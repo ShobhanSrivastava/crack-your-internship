7c944c67e9b7d6b59a08ee24b441c06ca215b6b2
ShobhanSrivastava <76567059+ShobhanSrivastava@users.noreply.github.com>
2022-09-19 22:24:47 +0530

    Time: 2 ms (75.55%), Space: 73.6 MB (57.25%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.java
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -1,4 +1,15 @@
 class Solution {
+    
+    /*
+        Using Kadane's algorithm
+        
+        - We start with the first element as the globalSum
+        - We add the elements to the localSum as long as the localSum is positive
+        - At each updation in the localSum, update the globalSum if the the localSum is greater than the globalSum
+        - If at any point the localSum becomes less than or equal to 0, there is no point taking it further,
+            because it will have no further contribution in the next subarray
+        - Return the globalSum
+    */
     public int maxSubArray(int[] nums) {
         int max=nums[0], sum=0;
         
