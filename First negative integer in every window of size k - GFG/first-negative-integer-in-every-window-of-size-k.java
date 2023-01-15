8b84df5889ab78d935176a6acb9cb7bef5f2f02d
ShobhanSrivastava <76567059+ShobhanSrivastava@users.noreply.github.com>
2023-01-15 21:39:01 +0530

    Added solution - LeetHub

---
+++ First negative integer in every window of size k - GFG/first-negative-integer-in-every-window-of-size-k.java
@@ -0,0 +1,79 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            long a[] = new long[(int)(n)];
+            // long getAnswer[] = new long[(int)(n)];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Long.parseLong(inputLine[i]);
+            }
+            int k = Integer.parseInt(br.readLine().trim());
+            
+            Compute obj = new Compute();
+            long answer[] = obj.printFirstNegativeInteger(a, n, k);
+            int sz = answer.length;
+            
+            StringBuilder output = new StringBuilder();
+            for(int i=0;i<sz;i++)
+                output.append(answer[i]+" ");
+            System.out.println(output);
+            
+        }
+	}
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Compute {
+    
+    public long[] printFirstNegativeInteger(long A[], int N, int K)
+    {
+        long[] result = new long[N-K+1];
+        int resultIndex = 0;
+        int left = 0, right = 0;
+        
+        Queue<Long> queue = new LinkedList<>();
+        
+        if(A[0] < 0) queue.offer(A[0]);
+        
+        while(right < N){
+            if(right-left+1 < K) {
+                right++;
+                if(right < N && A[right] < 0) queue.offer(A[right]);
+            }
+            else if(right-left+1 == K){
+                if(!queue.isEmpty()){
+                    result[resultIndex++] = queue.peek();
+                    
+                    if(A[left] == queue.peek()) queue.poll();
+                }
+                else{
+                    result[resultIndex++] = 0;
+                }
+                
+                left++;
+            }
+        }
+        
+        return result;
+    }
+}
