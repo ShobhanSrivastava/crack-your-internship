677469363ac952fde9c0094aad90207e50b8d500
ShobhanSrivastava <76567059+ShobhanSrivastava@users.noreply.github.com>
2023-04-08 10:12:47 +0530

    Added solution - LeetHub

---
+++ Make the array beautiful - GFG/make-the-array-beautiful.java
@@ -0,0 +1,73 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class IntArray {
+    public static int[] input(BufferedReader br, int n) throws IOException {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a) {
+        for (int e : a) System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a) {
+
+        for (int e : a) System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out);
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+
+            int n;
+            n = Integer.parseInt(br.readLine());
+
+            int[] arr = IntArray.input(br, n);
+
+            Solution obj = new Solution();
+            ArrayList<Integer> res = obj.makeBeautiful(arr);
+            for (int e : res) out.print(e + " ");
+            out.println();
+        }
+        out.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public static ArrayList<Integer> makeBeautiful(int[] arr) {
+        ArrayList<Integer> result = new ArrayList<>();
+        int len = arr.length;
+        Stack<Integer> stack = new Stack<>();
+        
+        for(int i=0 ; i<len ; i++) {
+            if(stack.isEmpty()) stack.push(arr[i]);
+            else {
+                int top = stack.peek();
+                
+                if((top >= 0 && arr[i] < 0) || (top < 0 && arr[i] >= 0)) stack.pop();
+                else stack.push(arr[i]);
+            }
+        }
+    
+        while(!stack.isEmpty()) result.add(stack.pop());
+        
+        Collections.reverse(result);
+        return result;
+    }
+}
